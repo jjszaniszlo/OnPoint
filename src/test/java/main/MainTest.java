594a5f9290735f526e103fba9c5c36c9a4647476
edwardeckelberry <edward.eckelberry@gmail.com>
2024-01-24 21:04:22 -0800

    testing out commit by adding hello world to MainTest.

--- src/test/java/main/MainTest.java
+++ src/test/java/main/MainTest.java
@@ -3,5 +3,7 @@ package main;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
