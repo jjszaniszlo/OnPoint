8913ffaa756d2d9d0077eaba96543caa8d93db49
jjszaniszlo <johnszaniszlo@gmail.com>
2024-01-31 16:15:05 -0800

    clear main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,8 +2,5 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bye-Bye World!");
-        System.out.println("Test2");
-        System.out.println("Test Vini");
     }
 }
