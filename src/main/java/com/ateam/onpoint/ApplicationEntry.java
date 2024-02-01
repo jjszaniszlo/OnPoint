f7e53a85fa79e376b1f6cb51a20b68dd0d7adf34
jjszaniszlo <johnszaniszlo@gmail.com>
2024-01-31 16:46:18 -0800

    basic application with JavaFX

--- src/main/java/com/ateam/onpoint/ApplicationEntry.java
+++ src/main/java/com/ateam/onpoint/ApplicationEntry.java
@@ -1,6 +1,34 @@
 package com.ateam.onpoint;
 
-public class ApplicationEntry {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ApplicationEntry extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("OnPoint");
+
+        Button btn = new Button();
+        btn.setText("Say 'Hello World!'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        stage.setScene(new Scene(root, 300, 250));
+        stage.show();
     }
 }
