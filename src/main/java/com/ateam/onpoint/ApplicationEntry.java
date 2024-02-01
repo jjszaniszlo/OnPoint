63ff3545751699e7481c2f64ab1430d3e74da945
jjszaniszlo <johnszaniszlo@gmail.com>
2024-01-31 17:14:49 -0800

    add javadoc and documentation for entry point

--- src/main/java/com/ateam/onpoint/ApplicationEntry.java
+++ src/main/java/com/ateam/onpoint/ApplicationEntry.java
@@ -8,11 +8,20 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+/*
+ * The class which the application is launched from.
+ */
+
 public class ApplicationEntry extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * The start point for the primary JavaFX stage.
+     * @param stage the primary stage
+     * @throws Exception
+     */
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("OnPoint");
