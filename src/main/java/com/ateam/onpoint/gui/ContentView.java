3264f51a203cee7d2fddeddacc3ecd120369f69e
jjszaniszlo <johnszaniszlo@gmail.com>
2024-03-25 02:56:20 -0700

    add ContentView.java

---
+++ src/main/java/com/ateam/onpoint/gui/ContentView.java
@@ -0,0 +1,10 @@
+package com.ateam.onpoint.gui;
+
+import com.ateam.onpoint.gui.content.IContent;
+
+/*
+ * Content view contains the panel where all the application's functionality will dynamically be made available through the sidebar.
+ */
+public class ContentView {
+    private Class<? extends IContent> selectedContent;
+}
