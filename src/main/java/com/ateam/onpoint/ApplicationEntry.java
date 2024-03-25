91f3973b0e848300d04358b2d2f837d6e58b10df
jjszaniszlo <johnszaniszlo@gmail.com>
2024-03-24 17:02:14 -0700

    cleanup ApplicaitonEntry.java and seperate scene creation in preperation for some property modifications

--- src/main/java/com/ateam/onpoint/ApplicationEntry.java
+++ src/main/java/com/ateam/onpoint/ApplicationEntry.java
@@ -1,13 +1,9 @@
 package com.ateam.onpoint;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /*
@@ -29,8 +25,9 @@ public class ApplicationEntry extends Application {
         stage.setTitle("OnPoint");
 
         Parent root = FXMLLoader.load(getClass().getResource("/main_layout.fxml"));
+        Scene rootScene = new Scene(root, 840, 640);
 
-        stage.setScene(new Scene(root, 840, 640));
+        stage.setScene(rootScene);
         stage.show();
     }
 }
