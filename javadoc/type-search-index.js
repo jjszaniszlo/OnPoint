aa700fe4635eaa10346905d09fac3288d887be9b
jjszaniszlo <40372435+jjszaniszlo@users.noreply.github.com>
2024-03-25 00:35:03 +0000

    Deploying to javadoc from @ jjszaniszlo/groupApplication@d26d58f931e28e63cec1ea0774cf4766b29930f3 🚀

--- javadoc/type-search-index.js
+++ javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.ateam.onpoint","l":"ApplicationEntry"},{"p":"com.ateam.onpoint","l":"TeamConventions"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.ateam.onpoint","l":"ApplicationEntry"}];updateSearchResults();
