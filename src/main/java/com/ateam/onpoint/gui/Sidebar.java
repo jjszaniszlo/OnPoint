7a431fd5a9fcab93ca945a891e795ac03352b32f
jjszaniszlo <johnszaniszlo@gmail.com>
2024-03-25 13:00:30 -0700

    add sidebar header component

--- src/main/java/com/ateam/onpoint/gui/Sidebar.java
+++ src/main/java/com/ateam/onpoint/gui/Sidebar.java
@@ -1,7 +1,11 @@
 package com.ateam.onpoint.gui;
 
-import com.ateam.onpoint.gui.components.UserHeader;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /*
@@ -9,9 +13,33 @@ import javafx.scene.layout.VBox;
  */
 public class Sidebar extends VBox {
     public Sidebar(ContentView contentView) {
-        // init a vbox
         super();
 
-        this.getChildren().addAll(new Button("Hello 1"),  new Button("Hello 2"));
+        this.getChildren().addAll(new Header());
+    }
+
+    /*
+     * The header has the user's name and profile photo, alongside the open settings functionality
+     */
+    private class Header extends HBox {
+        private final Button openProfile;
+        public Header() {
+            super();
+
+            openProfile = new Button("Your Name");
+            openProfile.setPrefWidth(OnPointGUI.SIDEBAR_WIDTH);
+            openProfile.setPrefHeight(60);
+            openProfile.getStyleClass().add("flat");
+            openProfile.setStyle("-fx-underline: false");
+            openProfile.setContentDisplay(ContentDisplay.LEFT);
+            openProfile.setAlignment(Pos.BASELINE_LEFT);
+
+            ImageView img = new ImageView(getClass().getResource("/img/placeholder.png").toExternalForm());
+            img.setPreserveRatio(true);
+            img.fitWidthProperty().bind(openProfile.widthProperty().divide(6));
+            openProfile.setGraphic(img);
+
+            this.getChildren().add(openProfile);
+        }
     }
 }
