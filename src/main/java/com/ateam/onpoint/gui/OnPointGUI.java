71ce9e1eeca66bc4d882159d6f25e503f17ca71c
jjszaniszlo <johnszaniszlo@gmail.com>
2024-03-25 02:56:51 -0700

    add OnPointGUI.java

--- src/main/java/com/ateam/onpoint/gui/OnPointGUI.java
+++ src/main/java/com/ateam/onpoint/gui/OnPointGUI.java
@@ -1,4 +1,22 @@
 package com.ateam.onpoint.gui;
 
-public class OnPointGUI {
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
+/*
+ * Manages all the components specific to the application
+ */
+public class OnPointGUI extends AnchorPane {
+    public int MIN_WINDOW_WIDTH = 1200;
+    public int SIDEBAR_WIDTH = 300;
+
+    public OnPointGUI() {
+        StackPane body = new StackPane();
+        body.getStyleClass().add("body");  // add css to all children of the stack pane
+
+        body.getChildren().add(new Dashboard());
+
+        this.getChildren().add(body);
+    }
 }
