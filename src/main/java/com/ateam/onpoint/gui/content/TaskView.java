842ceb196374d69cd3b7c0a1a6e912014cf9987b
jjszaniszlo <johnszaniszlo@gmail.com>
2024-03-25 13:01:11 -0700

    add class basis for TaskView.java

---
+++ src/main/java/com/ateam/onpoint/gui/content/TaskView.java
@@ -0,0 +1,10 @@
+package com.ateam.onpoint.gui.content;
+
+import javafx.scene.Parent;
+
+public class TaskView implements IContent {
+    @Override
+    public Parent getContentView() {
+        return null;
+    }
+}
