7d2b2f96878f09a8993057cda31dd3a9e928fa85
jjszaniszlo <johnszaniszlo@gmail.com>
2024-03-25 12:59:34 -0700

    begin NavTree class

---
+++ src/main/java/com/ateam/onpoint/gui/components/NavTree.java
@@ -0,0 +1,19 @@
+package com.ateam.onpoint.gui.components;
+
+import javafx.scene.control.TreeCell;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+public class NavTree extends TreeView<NavTree.Nav> {
+    public static class Nav {
+
+    }
+
+    public static class NavItem extends TreeItem<Nav> {
+
+    }
+
+    public static class NavCell extends TreeCell<Nav> {
+
+    }
+}
