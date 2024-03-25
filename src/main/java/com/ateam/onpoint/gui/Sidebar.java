f3a2dc7370adde426562296820cd794153b92058
jjszaniszlo <johnszaniszlo@gmail.com>
2024-03-25 15:14:47 -0700

    change the alignment and position of sidebar content

--- src/main/java/com/ateam/onpoint/gui/Sidebar.java
+++ src/main/java/com/ateam/onpoint/gui/Sidebar.java
@@ -1,9 +1,9 @@
 package com.ateam.onpoint.gui;
 
+import com.ateam.onpoint.gui.components.NavTree;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
-import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -12,10 +12,13 @@ import javafx.scene.layout.VBox;
  * Contains the user account snippet, and the navbar for app navigation.
  */
 public class Sidebar extends VBox {
-    public Sidebar(ContentView contentView) {
+    private final NavTree navTree;
+    public Sidebar(ContentView view) {
         super();
 
-        this.getChildren().addAll(new Header());
+        this.navTree = new NavTree(view);
+
+        this.getChildren().addAll(new Header(), navTree);
     }
 
     /*
@@ -26,20 +29,20 @@ public class Sidebar extends VBox {
         public Header() {
             super();
 
-            openProfile = new Button("Your Name");
-            openProfile.setPrefWidth(OnPointGUI.SIDEBAR_WIDTH);
-            openProfile.setPrefHeight(60);
-            openProfile.getStyleClass().add("flat");
-            openProfile.setStyle("-fx-underline: false");
-            openProfile.setContentDisplay(ContentDisplay.LEFT);
-            openProfile.setAlignment(Pos.BASELINE_LEFT);
+            this.openProfile = new Button("YOUR NAME");
+            this.openProfile.setContentDisplay(ContentDisplay.LEFT);
+            this.openProfile.setAlignment(Pos.BASELINE_LEFT);
+            this.openProfile.setPrefWidth(OnPointGUI.SIDEBAR_WIDTH);
+            this.openProfile.setPrefHeight(60);
+            this.openProfile.getStyleClass().add("flat");
+            this.openProfile.setStyle("-fx-underline: false");
 
             ImageView img = new ImageView(getClass().getResource("/img/placeholder.png").toExternalForm());
             img.setPreserveRatio(true);
-            img.fitWidthProperty().bind(openProfile.widthProperty().divide(6));
-            openProfile.setGraphic(img);
+            img.fitWidthProperty().bind(this.openProfile.widthProperty().divide(6));
+            this.openProfile.setGraphic(img);
 
-            this.getChildren().add(openProfile);
+            this.getChildren().add(this.openProfile);
         }
     }
 }
