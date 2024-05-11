07c268267d88301287c2b50ba2cece29c25f6e31
John Szaniszlo <Johnszaniszlo@gmail.com>
2024-05-10 23:13:07 -0700

    implement drag and drop

--- src/main/java/com/ateam/onpoint/core/TaskDatabase.java
+++ src/main/java/com/ateam/onpoint/core/TaskDatabase.java
@@ -1,4 +1,7 @@
 package com.ateam.onpoint.core;
 
+/*
+ * Handles saving tasks alongside other kinds of data to a local file format.
+ */
 public class TaskDatabase {
 }
--- src/main/java/com/ateam/onpoint/gui/components/TaskList.java
+++ src/main/java/com/ateam/onpoint/gui/components/TaskList.java
@@ -3,10 +3,15 @@ package com.ateam.onpoint.gui.components;
 
 import javafx.application.Platform;
 import javafx.geometry.Pos;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyCode;
+import javafx.scene.input.*;
 import javafx.scene.layout.HBox;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @SuppressWarnings("unchecked")
 public class TaskList extends ListView<TaskList.Task> {
@@ -41,7 +46,6 @@ public class TaskList extends ListView<TaskList.Task> {
             });
 
             this.descriptionField = createTextField();
-
             this.contextMenu = createContextMenu();
 
             this.root.setAlignment(Pos.CENTER_LEFT);
@@ -52,8 +56,80 @@ public class TaskList extends ListView<TaskList.Task> {
                 this.contextMenu.show(this.root.getScene().getWindow(), e.getScreenX(), e.getScreenY());
             });
 
+            setupDragAndDrop();
+
             this.root.getChildren().addAll(this.checkBox, this.descriptionField);
         }
+
+        private void setupDragAndDrop() {
+            this.root.setOnDragDetected(e -> {
+                if (getItem() == null) {
+                    return;
+                }
+
+                final var dragBoard = startDragAndDrop(TransferMode.MOVE);
+                final var clipBoard = new ClipboardContent();
+                clipBoard.putString(this.getItem().description);
+                dragBoard.setDragView(this.root.snapshot(new SnapshotParameters(), null));
+                dragBoard.setContent(clipBoard);
+
+                e.consume();
+            });
+
+            this.root.setOnDragOver(e -> {
+                if (e.getGestureSource() != this && e.getDragboard().hasString()) {
+                    e.acceptTransferModes(TransferMode.MOVE);
+                }
+                e.consume();
+            });
+
+            this.root.setOnDragEntered(e -> {
+                if (e.getGestureSource() != this && e.getDragboard().hasString()) {
+                    this.setOpacity(0.3);
+                }
+            });
+
+            this.root.setOnDragExited(e -> {
+                if (e.getGestureSource() != this && e.getDragboard().hasString()) {
+                    this.setOpacity(1);
+                }
+            });
+
+            this.root.setOnDragDropped(e -> {
+                if (this.getItem() == null) {
+                    return;
+                }
+
+                final var dragboard = e.getDragboard();
+                var success = false;
+
+                if (dragboard.hasString()) {
+                    final var items = getListView().getItems();
+                    final var draggedItem = items.stream()
+                            .filter(c -> c.description.compareTo(dragboard.getString()) == 0)
+                            .findFirst()
+                            .orElse(null);
+                    Objects.requireNonNull(draggedItem);
+
+                    final var draggedItemIndex = items.indexOf(draggedItem);
+                    final var thisIndex = items.indexOf(getItem());
+
+                    items.set(draggedItemIndex, getItem());
+                    items.set(thisIndex, draggedItem);
+
+                    List<Task> itemsCopy = new ArrayList<>(getListView().getItems());
+                    getListView().getItems().setAll(itemsCopy);
+
+                    success = true;
+                }
+
+                e.setDropCompleted(success);
+                e.consume();
+            });
+
+            setOnDragDone(DragEvent::consume);
+        }
+
         private @NotNull ContextMenu createContextMenu() {
             ContextMenu contextMenu = new ContextMenu();
 
