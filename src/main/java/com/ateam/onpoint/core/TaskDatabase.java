951f7321717df7148745ac0f4543578c34e4e5a2
John Szaniszlo <Johnszaniszlo@gmail.com>
2024-05-11 02:15:20 -0700

    - basic setup for TaskDatabase

--- src/main/java/com/ateam/onpoint/core/TaskDatabase.java
+++ src/main/java/com/ateam/onpoint/core/TaskDatabase.java
@@ -1,7 +1,25 @@
 package com.ateam.onpoint.core;
 
+import java.util.Date;
+
 /*
  * Handles saving tasks alongside other kinds of data to a local file format.
  */
 public class TaskDatabase {
+    private static TaskDatabase instance;
+
+    private TaskDatabase() {}
+
+    public TaskDatabase getInstance() {
+        if (instance == null) {
+            instance = new TaskDatabase();
+        }
+        return instance;
+    }
+
+    private static class TaskRecord {
+        private String description;
+        private Date creationDate;
+        private Date completionDate;
+    }
 }
