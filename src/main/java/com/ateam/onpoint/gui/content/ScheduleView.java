a84d5bbb9899a4624f97e0c27127181c1125b526
Dominic <dodiblasio@playstorygames.com>
2024-05-11 20:16:17 -0700

    ScheduleView setup pt.1

--- src/main/java/com/ateam/onpoint/gui/content/ScheduleView.java
+++ src/main/java/com/ateam/onpoint/gui/content/ScheduleView.java
@@ -1,18 +1,53 @@
 package com.ateam.onpoint.gui.content;
 
+
+// Package refs (com.ateam.onpoint.gui)
 import com.ateam.onpoint.gui.components.Spacer;
-//import javafx.geometry.Orientation;
+import com.ateam.onpoint.gui.components.TaskList;
+
+// javafx
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class ScheduleView extends VBox implements IContent {
+
+    private static TaskList scheduleTLToday;
+    private static TaskList scheduleTLTomorrow;
+    private static TaskList scheduleTLUpcoming;
+
+    public TaskList InitializeTL()
+    {
+        TaskList tl = new TaskList();
+        tl.setCellFactory(p -> new TaskList.TaskCell());
+        tl.setPrefWidth(130);
+        System.out.println("Successfully initialized " + tl.toString() + "!");
+        return tl;
+    }
+
+
     public ScheduleView() {
         super();
+
+        // Initialize the respective static TaskLists
+        if(scheduleTLToday == null)
+        {
+            scheduleTLToday = InitializeTL();
+        }
+        if(scheduleTLTomorrow == null)
+        {
+            scheduleTLTomorrow = InitializeTL();
+        }
+
+        if(scheduleTLUpcoming == null)
+        {
+            scheduleTLUpcoming = InitializeTL();
+        }
+
+
+
 
         this.setPrefWidth(400);
         this.setAlignment(Pos.TOP_CENTER);
@@ -25,7 +60,11 @@ public class ScheduleView extends VBox implements IContent {
         HBox datesContainer = new HBox();
         datesContainer.getChildren().addAll(todayLabel, new Spacer(), tomorrowLabel, new Spacer(), upcomingLabel);
 
-        this.getChildren().addAll(header, datesContainer);
+        // Content lists (similar to TaskView.java)
+        HBox scheduleTLs = new HBox();
+        scheduleTLs.getChildren().addAll(scheduleTLToday, new Spacer(), scheduleTLTomorrow, new Spacer(), scheduleTLUpcoming);
+
+        this.getChildren().addAll(header, datesContainer, scheduleTLs);
     }
 
     @Override
