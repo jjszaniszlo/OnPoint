9863cebd74ac485b8289ae1be262b46ecb2a68b9
John Szaniszlo <Johnszaniszlo@gmail.com>
2024-05-11 21:04:52 -0700

    add arrow and clock image

--- src/main/java/com/ateam/onpoint/gui/components/TaskCell.java
+++ src/main/java/com/ateam/onpoint/gui/components/TaskCell.java
@@ -1,6 +1,9 @@
 package com.ateam.onpoint.gui.components;
 
 import com.ateam.onpoint.core.Task;
+import com.ateam.onpoint.core.TaskDatabase;
+import com.ateam.onpoint.gui.OnPointGUI;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.control.*;
@@ -16,18 +19,23 @@ import java.util.List;
 import java.util.Objects;
 
 public class TaskCell extends ListCell<Task> {
-    private ImageView clockImage = new ImageView(Objects.requireNonNull(getClass().getResource("/img/clock_48.png")).toString());
     private final HBox root = new HBox();
     private final Label description = new Label();
     private final CheckBox checkBox = new CheckBox();
-    private final Label dateLabel = new Label("dd/mm/yyyy hh:mm");
+    private final Label dateLabel = new Label("yyyy/mm/dd hh:mm");
+    private final Label durationLabel = new Label("0h 0m");
     private final ContextMenu contextMenu;
 
     public TaskCell() {
         super();
 
-        this.clockImage.setFitWidth(20);
-        this.clockImage.setFitHeight(20);
+        ImageView clockImage = new ImageView(Objects.requireNonNull(getClass().getResource("/img/clock_48.png")).toString());
+        clockImage.setFitWidth(20);
+        clockImage.setFitHeight(20);
+
+        ImageView arrowImage = new ImageView(Objects.requireNonNull(getClass().getResource("/img/arrow_50.png")).toString());
+        arrowImage.setFitWidth(20);
+        arrowImage.setFitHeight(20);
 
         setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
         setAlignment(Pos.CENTER);
@@ -35,7 +43,11 @@ public class TaskCell extends ListCell<Task> {
         this.checkBox.setOnMouseClicked(e -> this.getItem().isCompletedProperty().set(this.checkBox.isSelected()));
 
         this.description.setStyle("-fx-font-weight: 600;");
-        this.description.setMaxWidth(200);
+        this.description.setPrefWidth(OnPointGUI.CONTENT_VIEW_WIDTH * 0.4);
+        this.description.setMinWidth(Control.USE_PREF_SIZE);
+        this.description.setMaxWidth(Control.USE_PREF_SIZE);
+
+        this.description.setWrapText(true);
 
         this.contextMenu = createContextMenu();
 
@@ -45,9 +57,22 @@ public class TaskCell extends ListCell<Task> {
 
         this.root.setOnContextMenuRequested(e -> this.contextMenu.show(this.root.getScene().getWindow(), e.getScreenX(), e.getScreenY()));
 
+        this.dateLabel.setPadding(new Insets(0, 5, 0,0));
+
         setupDragAndDrop();
 
-        this.root.getChildren().addAll(this.checkBox, this.description, new Spacer(), this.clockImage, new Spacer(5), this.dateLabel);
+        this.root.getChildren().addAll(
+                this.checkBox,
+                this.description,
+                new Spacer(),
+                durationLabel,
+                new Spacer(5),
+                arrowImage,
+                new Spacer(5),
+                clockImage,
+                new Spacer(5),
+                this.dateLabel
+        );
     }
 
     private void setupDragAndDrop() {
@@ -58,7 +83,7 @@ public class TaskCell extends ListCell<Task> {
 
             final var dragBoard = startDragAndDrop(TransferMode.MOVE);
             final var clipBoard = new ClipboardContent();
-            clipBoard.putString(this.getItem().getDescription());
+            clipBoard.putString(this.getItem().descriptionProperty().get());
             dragBoard.setDragView(this.root.snapshot(new SnapshotParameters(), null));
             dragBoard.setContent(clipBoard);
 
@@ -95,7 +120,7 @@ public class TaskCell extends ListCell<Task> {
             if (dragboard.hasString()) {
                 final var items = getListView().getItems();
                 final var draggedItem = items.stream()
-                        .filter(c -> c.getDescription().compareTo(dragboard.getString()) == 0)
+                        .filter(c -> c.descriptionProperty().get().compareTo(dragboard.getString()) == 0)
                         .findFirst()
                         .orElse(null);
                 Objects.requireNonNull(draggedItem);
@@ -128,7 +153,7 @@ public class TaskCell extends ListCell<Task> {
         });
 
         final var deleteTaskMenuItem = new MenuItem("Delete");
-        deleteTaskMenuItem.setOnAction(e -> this.getListView().getItems().remove(this.getListView().getSelectionModel().getSelectedItem()));
+        deleteTaskMenuItem.setOnAction(e -> TaskDatabase.getInstance().removeTask(this.getItem()));
 
         contextMenu.getItems().addAll(editTaskMenuItem, deleteTaskMenuItem);
         return contextMenu;
@@ -144,6 +169,23 @@ public class TaskCell extends ListCell<Task> {
         } else {
             this.description.setText(task.descriptionProperty().get());
             this.checkBox.setSelected(task.isCompletedProperty().get());
+
+            final var date = this.getItem().startDateProperty().get();
+            final var time = this.getItem().startTimeProperty().get();
+
+            if (date != null && time != null) {
+                this.dateLabel.setText(String.format(
+                        "%d/%02d/%02d %02d:%02d",
+                        date.getYear(), date.getMonthValue(), date.getDayOfMonth(), time.getHour(), time.getMinute())
+                );
+            }
+
+            final var duration = this.getItem().durationProperty().get();
+            if (duration != null) {
+                final int minutes = duration % 60;
+                final int hours = duration / 60;
+                this.durationLabel.setText(String.format("%02dh %02dm", hours, minutes));
+            }
 
             this.setGraphic(this.root);
         }
