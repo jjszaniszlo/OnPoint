9f8afdde6e979d7bc20989a589181de9275d5d22
John Szaniszlo <Johnszaniszlo@gmail.com>
2024-05-12 19:08:03 -0700

    Create ScheduleCell.java

--- src/main/java/com/ateam/onpoint/gui/components/ScheduleCell.java
+++ src/main/java/com/ateam/onpoint/gui/components/ScheduleCell.java
@@ -1,7 +1,31 @@
 package com.ateam.onpoint.gui.components;
 
+import com.ateam.onpoint.core.Task;
+import javafx.geometry.Pos;
+
 public class ScheduleCell extends Cell {
     public ScheduleCell() {
         super();
+
+        this.root.setAlignment(Pos.CENTER_LEFT);
+        this.root.prefHeight(30);
+        this.root.prefWidth(450);
+
+        this.root.getChildren().addAll(this.checkBox, this.description);
+    }
+
+    @Override
+    protected void updateItem(Task task, boolean empty) {
+        super.updateItem(task, empty);
+
+        if (task == null || empty) {
+            this.description.setText(null);
+            this.setGraphic(null);
+        } else {
+            this.description.setText(task.descriptionProperty().get());
+            this.checkBox.setSelected(task.isCompletedProperty().get());
+
+            this.setGraphic(this.root);
+        }
     }
 }
